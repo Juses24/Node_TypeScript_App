99d9c2778f1b0e351e2c54c798cf73c49bb9ed94
jesusdasilva2001 <Jesustamba2017@gmail.com>
2024-07-03 02:06:48 +0100

    testing the repository

---
+++ src/repositories/UserRepository.spec.ts
@@ -0,0 +1,27 @@
+import { EntityManager } from "typeorm"
+import { getMockEntityManager } from "../__mocks__/mockEntityManager.mock"
+import { User } from "../entities/User"
+import { UserRepository } from "./UserRepository"
+
+describe('UserRepository', () => {
+    let userRepository: UserRepository
+    let managerMock: Partial<EntityManager>
+
+    const mockUser: User = {
+        user_id: '12345',
+        name: 'Test User',
+        email: 'test@example.com',
+        password: 'password'
+
+    }
+
+    beforeAll(async () => {
+        const managerMock = await getMockEntityManager({})
+        userRepository = new UserRepository(managerMock as EntityManager)
+    })
+
+    it('Deve cadastrar um novo usuário no banco de dados', async () => {
+        await userRepository.createUser(mockUser)
+        //expect(managerMock.save).toHaveBeenCalled()
+    })
+})
--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,13 +1,12 @@
-import { EntityManager } from "typeorm";
-import { AppDataSource } from "../database";
-import { User } from "../entities/Users";
+import { EntityManager } from "typeorm"
+import { User } from "../entities/User";
 
 
 export class UserRepository {
     private manager: EntityManager
 
     constructor(
-        manager = AppDataSource.manager
+        manager: EntityManager
     ) {
         this.manager = manager;
     }
